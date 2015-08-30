97e2cb3b4b520e878867742b8e344592c649a525
Dina Nashaat <dina.nashaat@gmail.com>
2015-08-30 08:54:20 +0200

    Add association between Tasks and Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830050223) do
+ActiveRecord::Schema.define(version: 20150830065141) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "task"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20150830050223) do
     t.time     "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
